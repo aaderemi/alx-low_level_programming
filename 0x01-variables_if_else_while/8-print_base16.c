511edb07eef3046d883bfd632f2914670f206684
Adeola <aaderemi1@gmail.com>
2022-09-09 14:38:42 -0700

    added new stuff

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabets using putchar
+ *
+ * Return: 0 for success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; ++i)
+		putchar('0' + i);
+	for (i = 0; i < 6; ++i)
+		putchar('a' + i);
+	putchar('\n');
+	return (0);
+}
