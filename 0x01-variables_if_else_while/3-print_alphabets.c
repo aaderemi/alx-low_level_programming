d6730a738f3fcabb3ffe19cbea46ee455a13237e
Adeola <aaderemi1@gmail.com>
2022-09-09 13:37:06 -0700

    added new stuff

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabets using putchar
+ *
+ * Return: 0 for success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 26; ++i)
+		putchar('a' + i);
+	for (i = 0; i < 26; ++i)
+		putchar(65 + i);
+	putchar('\n');
+	return (0);
+}
