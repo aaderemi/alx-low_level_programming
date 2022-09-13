0259b903058a7361e0de94db502da843e3b0ccdf
Adeola <aaderemi1@gmail.com>
2022-09-13 13:15:37 -0700

    print az

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet  - prints a-z
+ *
+ * Return: function has not return
+ */
+void print_alphabet(void)
+{
+	const char alpha[27] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+	for (i = 0; i < 26; ++i)
+	{
+		_putchar(alpha[i]);
+	}
+	_putchar('\n');
+}
