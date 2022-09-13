5ea390ff2d57db791484d4df14f523ffd48efaff
Adeola <aaderemi1@gmail.com>
2022-09-13 16:34:16 -0700

    clock

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * jack_bauer - print all time
+ *
+ * Return: no return
+ */
+void jack_bauer(void)
+{
+	int h;
+	int m;
+
+	for (h = 0; h < 24; ++h)
+	{
+		for (m = 0; m < 60; ++m)
+		{
+			_putchar('0' + h/10);
+			_putchar('0' + h%10);
+			_putchar(':');
+			_putchar('0' + m/10);
+			_putchar('0' + m%10);
+			_putchar('\n');
+		}
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,3 +2,4 @@ int _putchar(char);
 int print_sign(int);
 int _abs(int);
 int print_last_digit(int);
+void jack_bauer(void);
