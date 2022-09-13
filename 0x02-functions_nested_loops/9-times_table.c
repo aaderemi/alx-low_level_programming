1a2efbbacf50816947806c80a593da5401254639
Adeola <aaderemi1@gmail.com>
2022-09-13 16:57:14 -0700

    9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * times_table - print all time table
+ *
+ * Return: no return
+ */
+void times_table(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; ++i)
+	{
+		for (j = 0; j < 10; ++j)
+		{
+			if (i * j < 10)
+			{
+				_putchar('0' + i * j);
+			}
+			else
+			{
+				_putchar('0' + (i * j) / 10);
+				_putchar('0' + (i * j) % 10);
+			}
+			if (j < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
