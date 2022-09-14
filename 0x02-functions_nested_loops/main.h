03dc74e21a56700145e1809e6b0d5cd0eb8c4d25
Adeola <aaderemi1@gmail.com>
2022-09-14 06:40:49 -0700

    added h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,3 +6,7 @@ void jack_bauer(void);
 void times_table(void);
 int add(int a, int b);
 void print_to_98(int a);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int);
+int _isalpha(int);
