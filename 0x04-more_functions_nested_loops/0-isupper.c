f3370506012e420bac709dcb8092a18fbb84e546
Adeola <aaderemi1@gmail.com>
2022-09-15 05:00:20 -0700

    upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include <ctype.h>
+#include "main.h"
+/**
+ * _isupper - checks if character is upper
+ * @c: input
+ *
+ * Return: 1 if true
+ */
+
+int _isupper(int c)
+{
+	return (isupper(c) ? 1 : 0);
+}
