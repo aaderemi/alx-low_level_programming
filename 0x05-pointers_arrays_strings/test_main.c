d4be973cca6ffa86756267699d1bea0e85d0e1ce
Adeola <aaderemi1@gmail.com>
2022-09-19 16:57:22 -0700

    hhh

--- 0x05-pointers_arrays_strings/test_main.c
+++ 0x05-pointers_arrays_strings/test_main.c
@@ -3,9 +3,10 @@
 #include <stdio.h>
 int main(void)
 {
-	char s1[98];
+	char s1[5] = {'H', 'H', 'H', 'H'};
 	char *ptr;
 
+	printf("%s\n", s1);
 	ptr = _strcpy(s1, "Z");
 	printf("%s, %ld\n", s1, strlen(s1));
 	printf("%s\n", ptr);
