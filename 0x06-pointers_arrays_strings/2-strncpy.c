8ed53e6bd7a52d0ee67e1c80380594281bf258d8
Adeola <aaderemi1@gmail.com>
2022-09-21 10:40:26 -0700

    str n cpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,37 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * @_strncpy - copies n characters
+ * @dest: to dest
+ * @src: from source
+ * @n: num char
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int len;
+	int i;
+	char *ret;
+
+	len = strlen(src);
+	i = 0;
+	ret = dest;
+
+	while (i < len && i < n)
+	{
+		*dest++ = *src++;
+		++i;
+	}
+	if (n - len > 0)
+	{
+		int j = 0;
+		int num_z = n - len;
+
+		for (; j < num_z; j++)
+			*dest++ = '\0';
+	}
+
+	return (ret);
+
+}
