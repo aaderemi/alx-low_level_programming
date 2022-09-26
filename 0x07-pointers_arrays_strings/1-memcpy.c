dce4c2c7ca25c918bccdd4cf3f1616af9053adbc
Adeola <aaderemi1@gmail.com>
2022-09-26 12:50:08 -0700

    memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _memcpy - copies memory
+ * @dest: to dest
+ * @src: from src
+ * @n: n bytes
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+	char *ret = dest;
+
+	for (i = 0; i < n; i++)
+	{
+		*(dest++) = src[i];
+	}
+
+	return (ret);
+}
