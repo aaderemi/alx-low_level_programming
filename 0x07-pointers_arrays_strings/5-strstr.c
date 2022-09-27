2d425ad1f489840f58341d4193779671c927cdaf
Adeola <aaderemi1@gmail.com>
2022-09-27 10:08:36 -0700

    eh

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -12,27 +12,38 @@ char *_strstr(char *haystack, char *needle)
 	char *ret = NULL;
 	char *nstart = needle;
 	unsigned long int lenneedle = strlen(needle);
+	unsigned long int j = 0;
+
+	if (!*needle)
+		return haystack;
 
 	while (*haystack)
 	{
+		if (j == lenneedle)
+			break;
 		if (ret == NULL && *haystack == *needle)
 		{
 			ret = haystack;
 			needle++;
+			j++;
 			haystack++;
 			continue;
 		}
 		if (ret && *haystack != *needle)
 		{
 			needle = nstart;
+			j = 0;
 			ret = NULL;
 		}
 		else if (ret && *haystack == *needle)
+		{
 			needle++;
+			j++;
+		}
 		haystack++;
 	}
 
-	if (ret)
+	if (ret != NULL)
 		ret = strlen(ret) >= lenneedle ? ret : NULL;
 	return (ret);
 }
--- 0x07-pointers_arrays_strings/main.c
+++ 0x07-pointers_arrays_strings/main.c
@@ -3,8 +3,8 @@
 
 int main(void)
 {
-	char *s = "hello, world";
-	char *f = "world";
+	char *s = "First, solve the problem. Then, write the code";
+	char *f = "";
 	char *t;
 
 	t = _strstr(s, f);
