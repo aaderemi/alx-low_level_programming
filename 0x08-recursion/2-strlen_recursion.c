21ed4b10bc5324395ebec42531b04b1ca9c058e8
Adeola <aaderemi1@gmail.com>
2022-09-28 14:24:22 -0700

    m

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
- * _strlen - returns strng length
+ * _strlen_recursion - returns strng length
  * @s: string
  * Return: length of string
  */
 int _strlen_recursion(char *s)
 {
 	int c = 0;
+
 	c = *s == 0 ? 0 : 1;
 	if (!*s)
 		return (0);
