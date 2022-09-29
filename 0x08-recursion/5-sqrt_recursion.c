543b8575a2985d84df46dc6d32b11447074a6c92
Adeola <aaderemi1@gmail.com>
2022-09-29 15:24:11 -0700

    betty

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,12 @@
 #include <stdlib.h>
 #include <math.h>
 #include "main.h"
+/**
+ * actual_fxn - does the sqrt. i used a lazy reqmt
+ * @est: initial estimate
+ * @n: number for which we need sqrt
+ * Return: sqrt(n)
+ */
 int actual_fxn(double est, int n)
 {
 	double newest = 0.5 * (est + n / est);
@@ -9,49 +15,11 @@ int actual_fxn(double est, int n)
 		return (newest);
 	return (actual_fxn(newest, n));
 }
-/*
-int _pow (int base, int exp)
-{
-	int ret = 1;
-
-	if (exp == 0)
-		return (1);
-	while (exp)
-	{
-		ret = ret * base;
-		exp--;
-	}
-	return (ret);
-}
-int estimate(int n)
-{
-	int evenpow = 0;
-	double val = (double)n;
-
-	while (val > 10.0)
-	{
-		val = val / 10.0;
-		evenpow++;
-	}
-	if (evenpow % 2 != 0)
-	{
-		if (val / 10 < 1)
-		{
-			val = val * 10;
-			evenpow--;
-		}
-		else
-		{
-			val = val / 10;
-			evenpow++;
-		}
-	}
-	if (val < 10)
-		return (2 * _pow(10, evenpow / 2));
-	else
-		return (6 * _pow(10, evenpow / 2));
-}
-*/
+/**
+ * _sqrt_recursion - wraper fxn for sqrt
+ * @n: same as above
+ * Return: same as above
+ */
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
