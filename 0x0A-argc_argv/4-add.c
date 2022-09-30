f0d2c25c47b3138c5ae41d19fdeb60bb699cf73b
Adeola <aaderemi1@gmail.com>
2022-09-30 15:28:23 -0700

    d

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -18,13 +18,23 @@ int main(int argc, char *argv[])
 	for (i = 1; i < argc; i++)
 	{
 		char *s = argv[i];
+		int to_be_added = atoi(s);
+		char str[12];
 
-		if (atoi(s) == 0 && (strlen(s) > 1 || (*s - '0' != 0)))
+		sprintf(str, "%d", to_be_added);
+
+		if (strlen(str) != strlen(s))
 		{
 			printf("Error\n");
 			return (1);
 		}
-		ret += atoi(argv[i]);
+
+		if (to_be_added == 0 && (strlen(s) > 1 || (*s - '0' != 0)))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		ret += to_be_added;
 	}
 	printf("%d\n", ret);
 	return (0);
