1b30e59744176881cedf204fb4b293842c248d53
Adeola <aaderemi1@gmail.com>
2022-10-06 09:03:23 -0700

    bett

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -31,7 +31,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		unsigned int a = strlen(s1);
 
 		ret = malloc(a + n + 1);
-		if(!ret)
+		if (!ret)
 			return (NULL);
 		if (n >= b)
 		{
