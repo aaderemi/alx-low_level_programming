25a362b38ed1fc7e89d7a394bc234e837dcd9d55
Adeola <aaderemi1@gmail.com>
2022-10-06 14:49:22 -0700

    m

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * array_range - python like range
+ * @min: min val
+ * @max: max value
+ * Return: pointer to array
+ */
+int *array_range(int min, int max)
+{
+	int *ret = malloc((max - min + 1) * sizeof(int));
+
+	if (min > max || ret == NULL)
+		return (NULL);
+	for (; min <= max; min++)
+	{
+		*ret = min;
+		ret++;
+	}
+	return (ret - (max - min - 1));
+}
